d2d6ff371984e3c10fa2917a6d238d14cf480c03
Corey Bennett <bennetco@mail.gvsu.edu>
2018-05-02 20:51:34 -0400

    more files

---
+++ HelloWorldCommon/HelloWorldCommon/DTO/HelloWorldDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorldCommon
+{
+    public class HelloWorldDTO
+    {
+        public string Message { get; set; }
+    }
+}
