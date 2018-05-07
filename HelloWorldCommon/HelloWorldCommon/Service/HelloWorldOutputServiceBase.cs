07a21ba0297ed44ab9c7018868b3460d3c801538
Corey Bennett <bennetco@mail.gvsu.edu>
2018-05-07 19:23:16 -0400

    proj changes

--- HelloWorldCommon/HelloWorldCommon/Service/HelloWorldOutputServiceBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using HelloWorldCommon.DTO;
-
-namespace HelloWorldCommon.Service
-{
-    public abstract class HelloWorldWriterBase : IHelloWorldWriter
-    {
-        public abstract void OutputHelloWorld(HelloWorldDTO content);
-    }
-}
